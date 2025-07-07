18dcc721b3624efb911460b7803cce230d58f268
saikat <saiskuila@gmail.com>
2025-07-07 20:05:18 +0530

    Added FE code for testimonial block

--- blocks/testimonial/testimonial.js
+++ blocks/testimonial/testimonial.js
@@ -1,13 +1,3 @@
 export default function decorate(block) {
-  const quote = block.querySelector('p');
-  const author = document.createElement('p');
-  author.className = 'testimonial-author';
-  author.textContent = block.dataset.author;
-
-  const designation = document.createElement('p');
-  designation.className = 'testimonial-designation';
-  designation.textContent = block.dataset.designation;
-
-  block.innerHTML = '';
-  block.append(quote, author, designation);
+  block.classList.add('testimonial');
 }
